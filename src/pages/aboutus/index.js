2786cd15cd5356c4e7f0097f56c8ddabdfed1344
Akash <akashrajmane23@gmail.com>
2023-12-27 12:54:16 +0530

    changes to aboutsus page

--- src/pages/aboutus/index.js
+++ src/pages/aboutus/index.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const About = () => {
   return (
-    <div style={{height:"100vh"}}>About Us Page</div>
+    <div style={{height:"100vh"}}>
+      <h1>About Us Page</h1>
+      <ul> 
+        <li>
+          <Link href="/aboutus/1">Yash</Link>
+        </li>
+        <li>
+          <Link href="/aboutus/2">Vaibhav</Link>
+        </li>
+        <li>
+          <Link href="/aboutus/3">Suresh</Link>
+        </li> 
+      </ul>
+     
+    </div>
   )
 }
 
